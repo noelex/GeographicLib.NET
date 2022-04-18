33bd1d8ce91cb47fb99c8b467e8291a941ce44e9
noelex <noelex@outlook.jp>
2022-04-18 10:49:48 +0800

    Add benchmark for Geodesic.GenDirect and Geodesic.GenInverse.

---
+++ GeographicLib.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace GeographicLib.Benchmarks
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<GeodesicBenchmark>();
+        }
+    }
+}
